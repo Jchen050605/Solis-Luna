f65c5006aded2eaf10c1c5b478dcae9b73fb783d
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-07-09 22:50:13 -0400

    adding index

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,18 +1,26 @@
-window.transitionToPage = function(href) {
+window.transitionToPage = function (href) {
     document.querySelector('body').style.opacity = 0
-    setTimeout(function() { 
+    setTimeout(function () {
         window.location.href = href
     }, 500)
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('body').style.opacity = 1
+document.addEventListener('DOMContentLoaded', function (event) {
+    if (window.location.href.includes("index")) {
+        setTimeout(function () {
+            document.querySelector('body').style.opacity = 1
+        }, 500)
+    }
+    else {
+        document.querySelector('body').style.opacity = 1
+    }
+    
 })
 
 document.querySelectorAll("a").forEach((elm) => {
     elm.addEventListener("click", (e) => {
         e.preventDefault();
-        if (elm.href.charAt(elm.href.length-1)=="#") return;
+        if (elm.href.charAt(elm.href.length - 1) == "#") return;
         transitionToPage(elm.href)
     })
 })

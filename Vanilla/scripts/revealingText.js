7e5dafd9d517b88f6aac840af8fded68d38ebd6f
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-07-10 23:23:44 -0400

    Updates

--- Vanilla/scripts/revealingText.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("scroll", () => {
-    document.querySelectorAll(".reveal").forEach((e) => {
-        if (window.innerHeight  > e.getBoundingClientRect().top)
-            e.classList.add("active");
-    });
-})

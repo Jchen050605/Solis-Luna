316cd5d0ba5dd1d1605d1c058fa5f171c55560bf
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-08-11 00:39:36 -0400

    region

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require( "express" );
+const express = require("express");
 const logger = require("morgan");
 const db = require('./firebase');
 const app = express();
 const port = 3000;
 
-app.set( "views",  __dirname + "/views");
-app.set( "view engine", "ejs" );
- 
-// app.use(logger("dev"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + '/public'));
 
@@ -34,24 +32,79 @@ async function formatRegions() {
         let documents = await db.collection("regions").doc("regions").collection(region).get()
         let documentNames = documents.docs.map(document => getFormattedId(document.id));
         if (documentNames.length == 1) {
-            formattedNavBar.push({"name":getFormattedId(region),"expanded":false})
+            formattedNavBar.push({ name: getFormattedId(region), expanded: false, id: region})
         }
         else {
-            formattedNavBar.push({"name":getFormattedId(region),"expanded":true,regions:documentNames})
+            formattedNavBar.push({ name: getFormattedId(region), expanded: true, regions: documentNames, id: region })
         }
     }
 
     return formattedNavBar
 }
 
-app.get( "/", async ( req, res ) => {
-    res.render('index', {regions: await formatRegions()})
-} );
+function getDaySuffix(day) {
+    if (day >= 11 && day <= 13) return 'th';
+    switch (day % 10) {
+        case 1: return 'st';
+        case 2: return 'nd';
+        case 3: return 'rd';
+        default: return 'th';
+    }
+}
+
+async function formatEvents(region) {
+    let eventIds = await region.collection('events').get()
+
+    let events = []
+
+    eventIds.docs.forEach((event) => {
+        let data = event.data()
+        let date = new Date(data.date._seconds * 1000)
+        let eventDate = date.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'});
+        let suffix = getDaySuffix(date.getDate());
+
+        eventDate.replace(/\d{1,2}$/, date.getDate() + suffix);
+        
+        events.push({ name: data.name, date: eventDate, description: data.description })
+    })
+
+    return events
+}
+
+async function formatPersons(region) {
+    let personIds = await region.collection('team').get()
+
+    let persons = []
+
+    personIds.docs.forEach((person) => {
+        let data = person.data()
+        
+        persons.push({ name: data.name, bio: data.bio, role: data.role })
+    })
+
+    return persons
+}
+
+app.get("/", async (req, res) => {
+    res.render('index', { regions: await formatRegions() })
+});
 
 app.get("/region/:region", async (req, res) => {
-    
+    let region = req.params.region
+
+    let regionList = await getRegionsId()
+
+    if (!regionList.includes(region)) {
+        res.redirect("/")
+    }
+    else {
+        let regionDB = await db.collection("regions").doc("regions").collection(region).get()
+        let documentID = regionDB.docs.map(document => document.id)[0];
+        regionDB = await db.collection("regions").doc("regions").collection(region).doc(documentID)
+        res.render('region', { regions: await formatRegions(), events: await formatEvents(regionDB), persons: await formatPersons(regionDB), name:getFormattedId(region) })
+    }
 })
 
-app.listen( port, () => {
-    console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
-} );
+app.listen(port, () => {
+    console.log(`App server listening on ${port}. (Go to http://localhost:${port})`);
+});
--- public/scripts/makePerson.js
+++ public/scripts/makePerson.js
@@ -46,37 +46,3 @@ function setRegularPage() {
 goback.addEventListener("click", () => {
     setRegularPage()
 })
-
-function createPerson(parentElm, name, pathToImage, bio, role=false) {
-    let personDiv = document.createElement("div")
-    personDiv.classList.add("person")
-    personDiv.classList.add("reveal")
-
-    let center = document.createElement("center")
-    
-    let profileImage = document.createElement("img")
-    profileImage.src = pathToImage
-    profileImage.classList.add("pfp")
-
-    center.appendChild(profileImage)
-
-    personDiv.appendChild(center)
-
-    let nameH5 = document.createElement("h5")
-    nameH5.textContent = name;
-    nameH5.classList.add("text-center")
-
-    personDiv.appendChild(nameH5)
-
-    let roleH6 = document.createElement('h6')
-    if (role!=false) {
-        roleH6.textContent = role
-        roleH6.classList.add("text-center")
-        personDiv.appendChild(roleH6)
-    }
-
-    parentElm.appendChild(personDiv)
-    personDiv.addEventListener("click", () => {setProfilePage(name, pathToImage)})
-
-    bios.set(name, bio)
-}
